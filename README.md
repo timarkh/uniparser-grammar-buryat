9375df8aaf0481ec4ecbba35bd33e83c285b071d
Timofey Arkhangelskiy <timarkh@gmail.com>
2021-12-13 22:53:36 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,44 @@
+# Buryat morphological analyzer
+This is a rule-based morphological analyzer for Buryat (``bua``; Mongolic). It is based on a formalized description of literary Buryat morphology, which uses [uniparser-morph](https://github.com/timarkh/uniparser-morph) for parsing. It performs full morphological analysis of Buryat words (lemmatization, POS tagging, grammatical tagging, glossing).
+
+## How to use
+### Python package
+The analyzer is available as a Python package. If you want to analyze Buryat texts in Python, install the module:
+
+```
+pip3 install uniparser-buryat
+```
+
+Import the module and create an instance of ``BuryatAnalyzer`` class. Set ``mode='strict'`` if you are going to process text in standard orthography, or ``mode='nodiacritics'`` if you expect some words to lack the diacritics (which often happens in social media). After that, you can either parse tokens or lists of tokens with ``analyze_words()``, or parse a frequency list with ``analyze_wordlist()``. Here is a simple example:
+
+```python
+from uniparser_buryat import BuryatAnalyzer
+a = BuryatAnalyzer(mode='strict')
+
+analyses = a.analyze_words('Морфологи')
+# The parser is initialized before first use, so expect
+# some delay here (usually several seconds)
+
+# You will get a list of Wordform objects
+# The analysis attributes are stored in its properties
+# as string values, e.g.:
+for ana in analyses:
+        print(ana.wf, ana.lemma, ana.gramm, ana.gloss)
+
+# You can also pass lists (even nested lists) and specify
+# output format ('xml' or 'json')
+# If you pass a list, you will get a list of analyses
+# with the same structure
+analyses = a.analyze_words([['А'], ['Би', 'шамдаа', 'дуратайб', '.']],
+	                       format='xml')
+analyses = a.analyze_words(['Морфологи', [['А'], ['Би', 'шамдаа', 'дуратайб', '.']]],
+	                       format='json')
+```
+
+Refer to the [uniparser-morph documentation](https://uniparser-morph.readthedocs.io/en/latest/) for the full list of options.
+
+### Word lists
+
+
+## Description format
+The description is carried out in the ``uniparser-morph`` format and involves a description of the inflection (paradigms.txt), a grammatical dictionary (bua_lexemes_XXX.txt files), and a short list of analyses that should be avoided (bad_analyses.txt). The dictionary contains descriptions of individual lexemes, each of which is accompanied by information about its stem, its part-of-speech tag and some other grammatical/borrowing information, its inflectional type (paradigm), and, for some, Russian translation. See more about the format [in the uniparser-morph documentation](https://uniparser-morph.readthedocs.io/en/latest/format.html).
---
+++ pre_build.py
@@ -0,0 +1,147 @@
+import re
+import os
+import shutil
+
+rxDiacritics = re.compile('[өүһё]')
+rxDiaPartsStem = re.compile('( stem:)( *[^\r\n]+)')
+rxDiaPartsFlex = re.compile('(-flex:)( *[^\r\n]+)')
+rxStemVariants = re.compile('[^ |/]+')
+rxFlexVariants = re.compile('[^ /]+')
+dictDiacritics = {
+    'ө': 'о',
+    'ү': 'у',
+    'һ': 'h',
+    'ё': 'е'
+}
+
+
+def collect_lemmata():
+    lemmata = ''
+    lexrules = ''
+    derivations = ''
+    for fname in os.listdir('.'):
+        if fname.endswith('.txt') and fname.startswith('bua_lexemes'):
+            f = open(fname, 'r', encoding='utf-8-sig')
+            lemmata += f.read() + '\n'
+            f.close()
+        elif fname.endswith('.txt') and fname.startswith('bua_lexrules'):
+            f = open(fname, 'r', encoding='utf-8-sig')
+            lexrules += f.read() + '\n'
+            f.close()
+        elif fname.endswith('.txt') and fname.startswith('bua_derivations'):
+            f = open(fname, 'r', encoding='utf-8-sig')
+            derivations += f.read() + '\n'
+            f.close()
+    lemmataSet = set(re.findall('-lexeme\n(?: [^\r\n]*\n)+', lemmata, flags=re.DOTALL))
+    # lemmata = '\n'.join(sorted(list(lemmataSet),
+    #                            key=lambda l: (re.search('gramm: *([^\r\n]*)', l).group(1), l)))
+    lemmata = '\n'.join(sorted(list(lemmataSet)))
+    return lemmata, lexrules, derivations
+
+
+def collect_paradigms():
+    fIn = open('paradigms.txt', 'r', encoding='utf-8-sig')
+    text = fIn.read()
+    fIn.close()
+    return text
+
+
+def add_diacriticless(morph):
+    """
+    Add a diacriticless variant to a stem or an inflection
+    """
+    morph = morph.group(0)
+    if rxDiacritics.search(morph) is None:
+        return morph
+    return morph + '//' + rxDiacritics.sub(lambda m: dictDiacritics[m.group(0)], morph)
+
+
+def process_diacritics_stem(line):
+    """
+    Remove diacritics from one line that contains stems.
+    """
+    morphCorrected = rxStemVariants.sub(add_diacriticless, line.group(2))
+    return line.group(1) + morphCorrected
+
+
+def process_diacritics_flex(line):
+    """
+    Remove diacritics from one line that contains inflections.
+    """
+    morphCorrected = rxFlexVariants.sub(add_diacriticless, line.group(2))
+    return line.group(1) + morphCorrected
+
+
+def simplify(text):
+    """
+    Add diacriticless variants for stems and inflections.
+    """
+    text = rxDiaPartsStem.sub(process_diacritics_stem, text)
+    text = rxDiaPartsFlex.sub(process_diacritics_flex, text)
+    return text
+
+
+def prepare_files():
+    """
+    Put all the lemmata to lexemes.txt. Put all the lexical
+    rules, if any, to lexical_rules.txt. Put all the derivations,
+    if any, derivations.txt. Create separate versions of
+    relevant files for diacriticless texts.
+    Put all grammar files to uniparser_buryat/data_strict/
+    (original version) or uniparser_buryat/data_nodiacritics/
+    (diacriticless version).
+    """
+    lemmata, lexrules, derivations = collect_lemmata()
+    paradigms = collect_paradigms()
+    fOutLemmata = open('uniparser_buryat/data_strict/lexemes.txt', 'w', encoding='utf-8')
+    fOutLemmata.write(lemmata)
+    fOutLemmata.close()
+    fOutLemmataNodiacritics = open('uniparser_buryat/data_nodiacritics/lexemes.txt', 'w', encoding='utf-8')
+    fOutLemmataNodiacritics.write(simplify(lemmata))
+    fOutLemmataNodiacritics.close()
+    if len(lexrules) > 0:
+        fOutLexrules = open('uniparser_buryat/data_strict/lex_rules.txt', 'w', encoding='utf-8')
+        fOutLexrules.write(lexrules)
+        fOutLexrules.close()
+        fOutLexrules = open('uniparser_buryat/data_nodiacritics/lex_rules.txt', 'w', encoding='utf-8')
+        fOutLexrules.write(lexrules)
+        fOutLexrules.close()
+    fOutParadigms = open('uniparser_buryat/data_strict/paradigms.txt', 'w', encoding='utf-8')
+    fOutParadigms.write(paradigms)
+    fOutParadigms.close()
+    fOutParadigmsNodiacritics = open('uniparser_buryat/data_nodiacritics/paradigms.txt', 'w', encoding='utf-8')
+    fOutParadigmsNodiacritics.write(simplify(paradigms))
+    fOutParadigmsNodiacritics.close()
+    fOutDerivations = open('uniparser_buryat/data_strict/derivations.txt', 'w', encoding='utf-8')
+    fOutDerivations.write(derivations)
+    fOutDerivations.close()
+    fOutDerivations = open('uniparser_buryat/data_nodiacritics/derivations.txt', 'w', encoding='utf-8')
+    fOutDerivations.write(derivations)
+    fOutDerivations.close()
+    if os.path.exists('bad_analyses.txt'):
+        shutil.copy2('bad_analyses.txt', 'uniparser_buryat/data_strict/')
+        shutil.copy2('bad_analyses.txt', 'uniparser_buryat/data_nodiacritics/')
+    if os.path.exists('buryat_disambiguation.txt'):
+        shutil.copy2('buryat_disambiguation.cg3', 'uniparser_buryat/data_strict/')
+        shutil.copy2('buryat_disambiguation.cg3', 'uniparser_buryat/data_nodiacritics/')
+
+
+def parse_wordlists():
+    """
+    Analyze wordlists/wordlist.csv.
+    """
+    from uniparser_buryat import BuryatAnalyzer
+    a = BuryatAnalyzer(mode='strict')
+    analyses = a.analyze_words([['ба'], ['Би', 'шамдаа', 'дуратайб', '.']],
+                               format='xml')
+    for ana in analyses:
+        print(ana)
+    a.analyze_wordlist(freqListFile='wordlists/wordlist.csv',
+                       parsedFile='wordlists/wordlist_analyzed.txt',
+                       unparsedFile='wordlists/wordlist_unanalyzed.txt',
+                       verbose=True)
+
+
+if __name__ == '__main__':
+    prepare_files()
+    parse_wordlists()
